3442da5cf4bdd5639b8dd34f3eff858c6a316fbe
hmanspie <hmanspie@gmail.com>
2022-11-28 23:10:54 +0200

    One

--- src/main/java/com/rebalcomb/control/RSAUtil.java
+++ src/main/java/com/rebalcomb/control/RSAUtil.java
@@ -9,62 +9,26 @@ public class RSAUtil {
     private final static BigInteger bitCount = new BigInteger("512");
 
     //todo Оголошення змінних необхідних для алгоритму RSA
-    private BigInteger p;
-    private BigInteger q;
-    private BigInteger module;
-    private BigInteger eulerNumber;
-    private BigInteger publicKey;
-    private BigInteger privateKey;
+
 
     //todo Конструктор (ініціалізація змінних)
     public RSAUtil(){
-
-        //todo p - псевдовипадкове просто число
-        p = getRandomPrimaryNumber();
-
-        //todo q - псевдовипадкове просто число
-        q = getRandomPrimaryNumber();
-
-        //todo module = p * q
-        module = getCulcModule();
-
-        //todo число Ейлера = (p - 1) * (q - 1)
-        eulerNumber = getEulerNumber();
-
-        //todo ініціалізаці публічного ключа
-        publicKey = genPublicKey();
-
-        //todo ініціалізаці приватного ключа
-        privateKey = genPrivateKey();
     }
 
     //todo публічний метод доступа для модуля
-    public BigInteger getModule() {
-        return module;
-    }
-
-    //todo публічний метод доступа для публічного ключа
-    public BigInteger getPublicKey() {
-        return publicKey;
-    }
-
-    //todo публічний метод доступа для приватного ключа
-    public BigInteger getPrivateKey() {
-        return privateKey;
-    }
 
     //todo Генерація псевдовипадкового простого числа
-    private BigInteger getRandomPrimaryNumber() {
+    public BigInteger getRandomPrimaryNumber() {
         return BigInteger.probablePrime(bitCount.intValue(), new SecureRandom());
     }
 
     //todo обчислення модуля
-    private BigInteger getCulcModule() {
+    public static BigInteger getCulcModule(BigInteger p, BigInteger q) {
         return p.multiply(q);
     }
 
     //todo обчислення числа Ейлера
-    private BigInteger getEulerNumber() {
+    public static BigInteger getEulerNumber(BigInteger p, BigInteger q) {
         BigInteger bigInteger = BigInteger.ONE;
         bigInteger = bigInteger.multiply(p.subtract(BigInteger.ONE));
         bigInteger = bigInteger.multiply(q.subtract(BigInteger.ONE));
@@ -72,7 +36,7 @@ public class RSAUtil {
     }
 
     //todo обчислення публічного ключа
-    public BigInteger genPublicKey() {
+    public static BigInteger genPublicKey(BigInteger eulerNumber) {
         BigInteger exponent;
         while (true) {
             exponent = BigInteger.probablePrime(bitCount.intValue(),  new SecureRandom());
@@ -83,7 +47,7 @@ public class RSAUtil {
     }
 
     //todo обчислення приватного ключа
-    public BigInteger genPrivateKey() {
+    public static BigInteger genPrivateKey(BigInteger publicKey, BigInteger eulerNumber) {
         return publicKey.modInverse(eulerNumber);
     }
 
--- src/main/java/com/rebalcomb/control/controller/CryptographyExam3.java
+++ src/main/java/com/rebalcomb/control/controller/CryptographyExam3.java
@@ -1,10 +1,13 @@
 package com.rebalcomb.control.controller;
 
 import com.rebalcomb.control.DataRequest;
+import com.rebalcomb.control.RSAUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.math.BigInteger;
 
 @Controller
 public class CryptographyExam3 {
@@ -28,9 +31,78 @@ public class CryptographyExam3 {
         return modelAndView;
     }
 
-    @PostMapping("/task1")
-    public ModelAndView postTask1(ModelAndView modelAndView){
-        modelAndView.setViewName("task1/ttask1");
+    @PostMapping("/task1Calculate")
+    public ModelAndView postTask1(ModelAndView modelAndView, DataRequest dataRequest){
+        String result = null;
+        if(dataRequest.getP() != "" && dataRequest.getQ() != "") {
+            BigInteger p = new BigInteger(dataRequest.getP());
+            BigInteger q = new BigInteger(dataRequest.getQ());
+            BigInteger module = RSAUtil.getCulcModule(p, q);
+            result = "n = p * q; \nn = " + p + " * " + q + " = " + module + "\n";
+            BigInteger eulerNumber = RSAUtil.getEulerNumber(p, q);
+            result += "phi(n) = (p - 1) * (q - 1); \nphi(n) = (" + p + " - 1) * (" + q + " - 1) = " + eulerNumber + "\n";
+            BigInteger publicKey = null;
+
+            if (dataRequest.getE() != "")
+                publicKey = new BigInteger(dataRequest.getE());
+            BigInteger privateKey = null;
+            if (dataRequest.getD() != "")
+                privateKey = new BigInteger(dataRequest.getD());
+            if (dataRequest.getE() == "" && dataRequest.getD() != "") {
+                publicKey = privateKey.modInverse(eulerNumber);
+                result += "e = d^-1mod(phi(n)); \ne = " + privateKey + " ^-1mod(" + eulerNumber + ") = " + publicKey + "\n";
+            }
+            if (dataRequest.getE() != "" && dataRequest.getD() == "") {
+                privateKey = publicKey.modInverse(eulerNumber);
+                result += "d = e^-1mod(phi(n)); \nd = " + publicKey + " ^-1mod(" + eulerNumber + ") = " + privateKey + "\n";
+            }
+            BigInteger M1 = null;
+
+            if (dataRequest.getM1() != "")
+                M1 = new BigInteger(dataRequest.getM1());
+            BigInteger C = null;
+
+            if (dataRequest.getC() != "")
+                C = new BigInteger(dataRequest.getC());
+            if (dataRequest.getM1() != "" && dataRequest.getC() == "") {
+                C = M1.modPow(publicKey, module);
+                result += "C = M^e mod(n); \nC = " + M1 + " ^ " + publicKey + " mod(" + module + ") = " + C + "\n";
+            } else if (dataRequest.getM1() == "" && dataRequest.getC() != "") {
+                M1 = C.modPow(privateKey, module);
+                result += "M = C^d mod(n); \nM = " + C + " ^ " + privateKey + " mod(" + module + ") = " + M1 + "\n";
+            } else if (dataRequest.getM1() != "" && dataRequest.getC() != "") {
+                C = M1.modPow(publicKey, module);
+                result += "C = M^e mod(n); \nC = " + M1 + " ^ " + publicKey + " mod(" + module + ") = " + C + "\n";
+                M1 = C.modPow(privateKey, module);
+                result += "M = C^d mod(n); \nM = " + C + " ^ " + privateKey + " mod(" + module + ") = " + M1 + "\n";
+            }
+
+            BigInteger M2 = null;
+            ;
+            if (dataRequest.getM2() != "")
+                M2 = new BigInteger(dataRequest.getM2());
+            BigInteger S = null;
+            ;
+            if (dataRequest.getS() != "")
+                S = new BigInteger(dataRequest.getS());
+
+            if (dataRequest.getM2() != "" && dataRequest.getS() == "") {
+                S = M2.modPow(privateKey, module);
+                result += "S = M^d mod(n); \nS = " + M2 + " ^ " + privateKey + " mod(" + module + ") = " + S + "\n";
+            } else if (dataRequest.getM2() == "" && dataRequest.getS() != "") {
+                M2 = S.modPow(publicKey, module);
+                result += "M = S^e mod(n); \nM = " + S + " ^ " + publicKey + " mod(" + module + ") = " + M2 + "\n";
+            } else if (dataRequest.getM2() != "" && dataRequest.getS() != "") {
+                S = M2.modPow(privateKey, module);
+                result += "S = M^d mod(n); \nS = " + M2 + " ^ " + privateKey + " mod(" + module + ") = " + S + "\n";
+                M2 = S.modPow(publicKey, module);
+                result += "M = S^e mod(n); \nM = " + S + " ^ " + publicKey + " mod(" + module + ") = " + M2 + "\n";
+            }
+        }
+
+        modelAndView.addObject("result", result);
+        modelAndView.setViewName("task1/task1");
+        modelAndView.addObject("dataRequest", new DataRequest());
         return modelAndView;
     }
 
