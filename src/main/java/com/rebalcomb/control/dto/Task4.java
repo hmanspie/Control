07ae9dbe7ecb95ff5d0af7ac96c307294571b6d3
Vladyslav <vladkalych@gmail.com>
2022-11-28 17:07:45 +0200

    Task4 - Done

---
+++ src/main/java/com/rebalcomb/control/dto/Task4.java
@@ -0,0 +1,23 @@
+package com.rebalcomb.control.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task4 {
+
+    private int x1;
+    private int y1;
+
+    private int x2;
+    private int y2;
+
+    private int x3;
+    private int y3;
+
+}
